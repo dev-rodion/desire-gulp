061907ac49e7d29c85f1dd3ab0627442d311318a
dev-rodion <gudkov.rodion.21@gmail.com>
2022-01-02 16:17:45 +0300

    Added blog__items, blog__blockquote and pagination blocks to blog.html, new font, styles, scripts and images

--- app/js/jq-main.js
+++ app/js/jq-main.js
@@ -15,6 +15,15 @@ $(function () {
     infinite: false
   });
 
+  $(".blog__item-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    prevArrow: "<button class='slider-arrow slider-arrow--prev'><img src='images/arrow-back.svg' alt='prev'/></button>",
+    nextArrow: "<button class='slider-arrow slider-arrow--next'><img src='images/arrow-next.svg' alt='next'/></button>"
+  });
+
   if ($(".gallery__inner").length !== 0) {
     const galleryMixer = mixitup(".gallery__inner", {
       load: {
