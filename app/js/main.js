2583aa31d86373fa98ffbda4c727f7899e7e8ccc
dev-rodion <gudkov.rodion.21@gmail.com>
2022-01-03 14:43:36 +0300

    Adapted header and menu, added menu button, new styles and scripts

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,8 @@ window.onload = function () {
   const rightsideMenu = document.querySelector(".rightside-menu");
   const rightsideClose = document.querySelector(".rightside-menu__close");
   const darkBg = document.querySelector(".dark-background");
+  const menuBtn = document.querySelector(".header__btn-menu");
+  const menus = document.querySelectorAll(".menu");
 
   headerBtn.addEventListener("click", () => {
     rightsideMenu.classList.remove("rightside-menu--close");
@@ -16,9 +18,42 @@ window.onload = function () {
   });
 
   darkBg.addEventListener("click", () => {
-    rightsideMenu.classList.add("rightside-menu--close");
+
+    if (!rightsideMenu.classList.contains("rightside-menu--close")) {
+      rightsideMenu.classList.add("rightside-menu--close");
+    }
+
+    if (menuBtn.classList.contains("header__btn-menu--active")) {
+      hideMenu()
+    }
+    
     hideDarkBg()
   })
+
+  menuBtn.addEventListener("click", function () {
+    if (this.classList.contains("header__btn-menu--active")) {
+      hideMenu()
+    } else {
+      showMenu()
+    }
+  })
+
+  function showMenu() {
+    for (const menu of menus) {
+      menu.classList.add('menu--open')
+    }
+    menuBtn.classList.add('header__btn-menu--active')
+    body.style.overflow = "hidden";
+    showDarkBg()
+  }
+  function hideMenu() {
+    for (const menu of menus) {
+      menu.classList.remove('menu--open')
+    }
+    menuBtn.classList.remove('header__btn-menu--active')
+    body.style.overflow = "";
+    hideDarkBg()
+  }
 
   function showDarkBg() {
     body.style.overflow = "hidden";
